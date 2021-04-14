f1074b3143e17da9cfad7f3a5af64f08a927c1b2
SPGrupo3 <82410064+SPGrupo3@users.noreply.github.com>
2021-04-14 16:06:50 -0300

    Update AlteraSenhaTest.java

--- src/test/java/br/com/proway/senior/controleAcesso/AlteraSenhaTest.java
+++ src/test/java/br/com/proway/senior/controleAcesso/AlteraSenhaTest.java
@@ -12,24 +12,25 @@ public class AlteraSenhaTest {
 		assertEquals(result, true);
 	}
 	
+	@Test
 	public void testValidarCodigoDiferente() {
 		boolean result = AlteraSenha.validarCodigo(54321, 12345);
 		assertEquals(result, false);
 	}
 	
+	@Test
 	public void testGerarCodigo5Digitos() {
 		int codigo = AlteraSenha.gerarCodigo();
 		int res = ("" + codigo).length();
 		assertEquals(5, res);	
 	}
-	
+	@Test
 	public void testSolicitaCodigo() {
 		assertEquals(AlteraSenha.solicitarCodigo(), 12345);
 	}
-	
+	@Test
 	public void testVerificarUsuarioExistente() {
 		String email = "teste@gmail.com";
 		assertEquals(AlteraSenha.verificarUsuario(email), true);
 	}
-
 }
